94741c59767a9f0681a36a3293094b607d979165
Emile Blouin <emile@Emiles-MacBook-Pro.local>
2019-03-08 18:32:40 -0800

    Added trivia

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 const hangman = new Card("Hangman", "A basic hangman game built from HTML, CSS, and JavaScript", "assets/images/Hangman.jpg", "https://eblouin876.github.io/Word-Guess-Game/")
 const starWarsRPG = new Card("Star Wars RPG", "Basic Star Wars RPG built with HTML, CSS, JavaScript, and JQuery", "assets/images/star-wars.jpg", "https://eblouin876.github.io/unit-4-game/")
-
-let portfolio = [hangman, starWarsRPG]
+const Trivia = new Card("Trivia Game", "A basic trivia game with multiple categories of questions to choose from built off of an open source trivia API", "assets/images/trivia.jpeg", "https://eblouin876.github.io/TriviaGame/")
+let portfolio = [hangman, starWarsRPG, Trivia]
 
 portfolio.forEach(function (each) {
     each.makeCard("portfolio-container")
